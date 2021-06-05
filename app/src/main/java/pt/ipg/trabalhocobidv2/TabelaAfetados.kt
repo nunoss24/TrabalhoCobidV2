69f61ab972b278ccb2e07412da132bcfe7d94653
nunoss24 <nunoabelss2424@gmail.com>
2021-06-05 17:18:36 +0100

    Adicionadas constantes para o nome das tabelas e dos campos

--- app/src/main/java/pt/ipg/trabalhocobidv2/TabelaAfetados.kt
+++ app/src/main/java/pt/ipg/trabalhocobidv2/TabelaAfetados.kt
@@ -1,11 +1,31 @@
 package pt.ipg.trabalhocobidv2
 
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 
 class TabelaAfetados (db: SQLiteDatabase) {
     private val db: SQLiteDatabase = db
 
     fun cria() {
-        db.execSQL("CREATE TABLE afetados(_id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, autor TEXT NOT NULL, telemovel INTEGER NOT NULL)")
+        db.execSQL(
+            "CREATE TABLE " + NOME_TABELA + "(" +
+                    BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    CAMPO_NOME + " TEXT NOT NULL, " +
+                    CAMPO_ENDERECO + " TEXT NOT NULL, " +
+                    CAMPO_NECESSIDADE + " TEXT NOT NULL, " +
+                    CAMPO_DATA_NASCIMENTO + " INTEGER NOT NULL, " +
+                    CAMPO_TELEMOVEL + " INTEGER NOT NULL, " +
+                    CAMPO_DESCRCAO_PROBLEMA + " TEXT NOT NULL, " +
+                    ")")
+    }
+
+    companion object {
+        const val NOME_TABELA = "afetados"
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_ENDERECO = "endereco"
+        const val CAMPO_NECESSIDADE = "necessidade"
+        const val CAMPO_DATA_NASCIMENTO = "data nascimento"
+        const val CAMPO_TELEMOVEL = "telemovel"
+        const val CAMPO_DESCRCAO_PROBLEMA = "Descrcao do problema"
     }
 }
--- app/src/main/java/pt/ipg/trabalhocobidv2/TabelaVoluntarios.kt
+++ app/src/main/java/pt/ipg/trabalhocobidv2/TabelaVoluntarios.kt
@@ -7,6 +7,23 @@ class TabelaVoluntarios (db: SQLiteDatabase) {
     private val db: SQLiteDatabase = db
 
     fun cria() {
-        db.execSQL("CREATE TABLE voluntarios(_id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL)")
+        db.execSQL("CREATE TABLE " + NOME_TABELA + "(" +
+                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                CAMPO_NOME + " TEXT NOT NULL, " +
+                CAMPO_LOCALIZACAO + " TEXT NOT NULL, " +
+                CAMPO_EMAIL + " TEXT NOT NULL, " +
+                CAMPO_DISPONIBILIDADE + " BOOLEAN NOT NULL, " +
+                CAMPO_TELEMOVEL + " INTEGER NOT NULL, " +
+                ")")
+    }
+
+    companion object {
+        const val NOME_TABELA = "voluntarios"
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_LOCALIZACAO = "endereco"
+        const val CAMPO_EMAIL = "necessidade"
+        const val CAMPO_DISPONIBILIDADE = "data nascimento"
+        const val CAMPO_TELEMOVEL = "telemovel"
+
     }
 }
