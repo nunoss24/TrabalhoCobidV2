7bc4498a6a9b10d26a95e08c0329fbcc994bafd6
nunoss24 <nunoabelss2424@gmail.com>
2021-06-05 16:17:18 +0100

    Criada classe para abrir a base de dados

---
+++ app/src/main/java/pt/ipg/trabalhocobidv2/BdCovidOpenHelper.kt
@@ -0,0 +1,20 @@
+package pt.ipg.trabalhocobidv2
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BdCovidOpenHelper (context: Context?)
+    : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
+    override fun onCreate(db: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+
+    companion object {
+        const val NOME_BASE_DADOS = "covid.db"
+        const val VERSAO_BASE_DADOS = 1
+    }
+}
